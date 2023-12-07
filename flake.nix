50bc1c4dc0239c44422c7b6ff49d2eddc7349b91
Nikodem Rabuliński <nikodem@rabulinski.com>
2023-12-07 20:15:11 +0100

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  inputs = {
+    aoc-utils.url = "github:nrabulinski/aoc-utils";
+    nixpkgs.follows = "aoc-utils/nixpkgs";
+    fenix.follows = "aoc-utils/fenix";
+  };
+
+  outputs = {
+    nixpkgs,
+    fenix,
+    aoc-utils,
+    ...
+  }: let
+    forEachSystem = f:
+      with nixpkgs.lib; let
+        systems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+      in
+        genAttrs systems (system: f nixpkgs.legacyPackages.${system});
+  in {
+    devShells = forEachSystem (pkgs: {
+      default = pkgs.mkShell {
+        packages =
+          [
+            fenix.packages.${pkgs.system}.complete.toolchain # rust toolchain
+            aoc-utils.packages.${pkgs.system}.aoc-cli
+          ]
+          ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [pkgs.libiconv];
+      };
+    });
+
+    formatter = forEachSystem (pkgs: pkgs.alejandra);
+  };
+}
---
+++ readme.md
@@ -0,0 +1,2 @@
+## philosophy
+First commit of a given day is *always* my first solution, which gave me the stars for said day. Only subsequent commits are allowed to change it/make it more pretty/optimize it/you-name-it
