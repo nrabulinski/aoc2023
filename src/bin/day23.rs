14f995bb711d4bc8e61f6894e2164544d3032a41
Nikodem Rabuliński <nikodem@rabulinski.com>
2023-12-23 17:37:01 +0100

    Big brain day 23

--- src/bin/day23.rs
+++ src/bin/day23.rs
@@ -1,4 +1,13 @@
-use std::collections::{BinaryHeap, HashMap, HashSet};
+use std::{
+	collections::{BinaryHeap, HashMap, HashSet},
+	sync::{
+		atomic::{AtomicBool, Ordering},
+		mpsc::{channel, Sender},
+		Arc,
+	},
+	thread,
+	time::Duration,
+};
 
 use aoc_lib::{
 	aoc,
@@ -44,69 +53,134 @@ fn part1(input: &str) -> Result<i64> {
 	Ok(*dist.get(&end).unwrap())
 }
 
-// This takes forever to finish, I have no clue why
-// (though it does finish eveutnaully),
-// so I just always print the longest path "so far".
-// I then left this running for like a minute, put it in as the answer,
-// and it turned out to be correct.
 fn part2(input: &str) -> Result<i64> {
 	let grid = Grid::for_str(input).unwrap();
 
 	let start = (1, 0);
 	let end = (grid.width() - 2, grid.height() - 1);
-	let mut largest = 0;
 
-	fn dfs(
-		mut curr: Point,
-		mut d: i64,
-		came_from: &HashSet<Point>,
-		grid: Grid<'_>,
-		looking_for: Point,
-		largest: &mut i64,
-	) -> i64 {
-		let mut came_from = came_from.clone();
-		loop {
-			if curr == looking_for {
-				if d < *largest {
-					return -1;
-				} else {
-					*largest = d;
-					println!("got here: {d}");
-					return d;
-				}
-			}
-			came_from.insert(curr);
+	let (tx, rx) = channel();
+	let should_stop = Arc::new(AtomicBool::new(false));
 
-			let mut neighbors = grid
-				.orthogonal_pos(curr)
-				.filter(|pos| !came_from.contains(pos) && grid[*pos] != b'#')
-				.peekable2();
-
-			match neighbors.peek_pair() {
-				(None, None) => return -1,
-				(Some(_), Some(_)) => {
-					let res = neighbors
-						.map(|neighbor| {
-							dfs(neighbor, d + 1, &came_from, grid, looking_for, largest)
-						})
-						.max()
-						.unwrap();
-					if res > *largest {
-						*largest = res;
-						println!("got here: {res}");
+	let res = thread::scope(|s| {
+		let t = s.spawn(|| {
+			fn dfs(
+				mut curr: Point,
+				mut d: i64,
+				came_from: &HashSet<Point>,
+				grid: Grid<'_>,
+				looking_for: Point,
+				largest: &mut i64,
+				sender: Sender<i64>,
+				should_stop: &AtomicBool,
+			) -> i64 {
+				let mut came_from = came_from.clone();
+				loop {
+					if should_stop.load(Ordering::Acquire) {
+						return -1;
 					}
-					return res;
-				}
-				(Some(&neighbor), None) => {
-					curr = neighbor;
-					d += 1
-				}
-				_ => unreachable!(),
-			}
-		}
-	}
+					if curr == looking_for {
+						if d > *largest {
+							*largest = d;
+							sender.send(d).unwrap();
+						}
+						return d;
+					}
+					came_from.insert(curr);
 
-	Ok(dfs(start, 0, &HashSet::new(), grid, end, &mut largest))
+					let mut neighbors = grid
+						.orthogonal_pos(curr)
+						.filter(|pos| !came_from.contains(pos) && grid[*pos] != b'#')
+						.peekable2();
+
+					match neighbors.peek_pair() {
+						(None, None) => return -1,
+						(Some(_), Some(_)) => {
+							let res = neighbors
+								.map(|neighbor| {
+									dfs(
+										neighbor,
+										d + 1,
+										&came_from,
+										grid,
+										looking_for,
+										largest,
+										sender.clone(),
+										should_stop,
+									)
+								})
+								.max()
+								.unwrap();
+							if res > *largest {
+								*largest = res;
+								sender.send(d).unwrap();
+							}
+							return res;
+						}
+						(Some(&neighbor), None) => {
+							curr = neighbor;
+							d += 1
+						}
+						_ => unreachable!(),
+					}
+				}
+			}
+			let mut largest = 0;
+			let should_stop = Arc::clone(&should_stop);
+			dfs(
+				start,
+				0,
+				&HashSet::new(),
+				grid,
+				end,
+				&mut largest,
+				tx,
+				&should_stop,
+			);
+		});
+		let mut largest = 0;
+
+		// Let's just say that if we don't receive a higher value for more than three minutes we found the answer
+		//                              ░▒▒▒▒▒▒
+		//                           ░▒▒░░░░░░░░░▒░
+		//                          ░▒░░░░░░░░░░░░░░
+		//                          ▒░░░░░░░░░░░░░░░
+		//                          ▒▒░░░░░░░░░░░░░░
+		//                         ▒▒▒░░░░░░░░░░░░░▒
+		//              ░▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒░░░░░░░▒▒▒
+		//          ▒▒░░░░▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+		//       ▒▒▒▒▒▒▒▒▒▒░▒░▒░▒▒▒▒▒▒░░░░▒▒▒▒▒
+		//     ▒▒▒▒▒▒░▒░░░░░░░░░░░░░▒░░░░░░░░▒▒     ▒▒▒▒▒▒▒▒▓▓▓▒▒▒▒▒▒▒
+		//   ░▒▒▒▒░░░░░░░░░░░░░░░░░░░░░░░░░░░░▒▒▒▒▒▒▒▒░░░▓▒░░░░░░░░░░▒▒▒▒▒▒
+		//  ▒▒▒▒▒░▒░░░░░░░░░░▒▒░░░░░░░░░░░░░░░░░▒▒░░░░░░░░░░░░░░░░░░░░░░░▓▓▓▒▒
+		//  ▒▒▒▒░▒░░░░░░░░░░░░░░░░░▒▒░░░░░░░░░░░▒▒░░░░░░░░░░░░░░░░░░░░░░░░░▒▓▒▒▒
+		// ▒▒▒▒░░░░░░░░░░░░░░░░░░░░░░░░▒░░░▒▒░░░░▒░░░░░▒▒██▒▓█▒░░░░░░░░░░░░░░▓▒▒▒▒
+		// ▒▒▒▒░▒░░░░░░░░░░░░░░░░░░░░░░░░░░▒░▒▒▒▒░░░░░▒█░░░░░░░░▒░░░░▒▒▒▒▒▒▒░░░▒▒▒▒▒
+		// ▒▒▒▒░░░░░░░░░░░░░░░░░░░░░░░░░░░░▒░▒▒▒▒░░░░▒█░ ▓ ▒▒█ ░█░░░▒█░░░░░░█▒░░▒▒▒▒▒
+		//  ▒▒▒▒░░░░░░░░░░░▒░░░░░░░░░░░░░░░░░▒▒░░░░▒▒░▓░█▒███▒ ░▒░░░█░░▒▒█   ░░░░▒▒▒▒▒
+		//  ▒▒▒▒▒░▒░░░░░░░░░░░░░▒░░░░░░░░░░░░░▒▒░░░░░▒▒▒▒█▓▒▓ ░░▒░░░▒████▒█  ░█▒▒▒▒▒▒▒
+		//   ▒▒▒▒▒░░░░░░░░░░░░░░░░░▒░░░░░░░░░▒▒░▒▒▒▒░▒▒▒░░░░░░░▒░░░░░▓█▒▒▓  ░░▓▒▒▒▒▒▒▒▒
+		//     ▒▒▒▒▒░▒░░░░░░░░░░░░░░░▒▒▒▒▒▒▒▒░░░░░░░░░░░░░░░▒░░░░░░░░░░▓░░░░▒▒▒▒░▒▒▒▒▒▒
+		//       ▒▒▒▒▒▒▒░▒░░░░░░░░░░░▒▒▒▒▒▒▒▒░░░░░░░░░▓░░░░░▒░░░░░░░░░░░▒░▒▒▒▒▒▒▒▒▒▒▒▒▒
+		//          ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒░▒░░░░░░░░░█░░▒░░░░░░░░░░░░░░░░░░▒▒░▒▒▒▒▒▒▒
+		//              ░▒▒▒▒▒▒▒▒▒▒▒░▒░░▒▒▒▒▒░▒░░░░░░░░▒██░░░░░░▓▒▒▒▒▒▒▒▒▒▒░░▒▒▒▒▒▒▒▒▒
+		//                           ▒▒░░░▒▒▒▒▒░▒░░░░░░░████▓   ░░░░░██░░░░░▒░▒▒▒▒▒▒▒░
+		//                            ▒▒░░░░▒▒▒▒░▒░░░░░░░▓▒███████████░░░▒░▒▒▒▒▒▒▒▒▒░
+		//                             ░▒░░░░░▒▒▒▒▒░░░░░░░▒▓▓▓▒██████▒░▒░▒▒▒▒▒▒▒▒▒▒
+		//                               ▒▒░░░░░▒▒▒▒▒▒▒░▒░░░▒▓▓▓▓██▒░▒▒▒▒▒▒▒▒▒▒▒▒▒
+		//                                 ▒▒░░░░░░▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+		//                                    ▒▒░░░░░▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+		//                                       ░▒▒░░░░▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒░
+		//                                             ░▒▒▒▒▒▒▒▒▒▒▒
+		while let Ok(dist) = rx.recv_timeout(Duration::from_secs(180)) {
+			largest = dist;
+		}
+		should_stop.store(true, Ordering::Release);
+		t.join().unwrap();
+		largest
+	});
+
+	Ok(res)
 }
 
 #[allow(dead_code)]
